21f422b613579b2b8eb283b9ab8bf7d342310c32
Smorcinski <pahblosmorcinski@gmail.com>
2024-05-25 01:32:05 -0300

    Testes: Testes de novo Script

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     const tooltip = document.getElementById('tooltip');
-    const pois = document.querySelectorAll('.poi');
-
-    pois.forEach(poi => {
-        poi.addEventListener('mouseenter', function () {
-            const info = this.getAttribute('data-info');
-            tooltip.innerHTML = info;
-            tooltip.style.display = 'block';
+    var pois = document.querySelectorAll('.poi');
+	
+        pois.forEach(function(poi) {
+            poi.addEventListener('click', function() {
+                var info = this.dataset.info;
+                var tooltip = document.getElementById('tooltip');
+                tooltip.textContent = info;
+                tooltip.style.display = 'block';
+                tooltip.style.top = (event.clientY - 200) + 'px'; // Posiciona a caixa branca verticalmente
+                tooltip.style.left = (event.clientX + 20) + 'px'; // Posiciona a caixa branca horizontalmente
+            });
         });
 
-        poi.addEventListener('mousemove', function (e) {
-            tooltip.style.top = e.clientY + 10 + 'px';
-            tooltip.style.left = e.clientX + 10 + 'px';
+        document.addEventListener('click', function(event) {
+            var tooltip = document.getElementById('tooltip');
+            if (!event.target.classList.contains('poi')) {
+                tooltip.style.display = 'none';
+            }
         });
-
-        poi.addEventListener('mouseleave', function () {
-            tooltip.style.display = 'none';
-        });
-    });
 });
