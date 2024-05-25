7da1ff06f1734594647ef224e8e079afca0cc00e
Smorcinski <pahblosmorcinski@gmail.com>
2024-05-25 11:03:59 -0300

    Atualizações: Atualizando cards

--- script.js
+++ script.js
@@ -15,10 +15,11 @@ document.addEventListener('DOMContentLoaded', function () {
 					card.style.left = mouseX + 'px'; // Define a posição horizontal do card
 
 					// Adiciona um evento de clique ao botão dentro do card
+					var cardName = target.replace(/\s+/g, '_'); // Substitui espaços por underscores
 					var linkButton = card.querySelector('.link-button');
 					if (linkButton) {
 						linkButton.addEventListener('click', function() {
-							window.location.href = 'https://noiteterna-rpg.fandom.com/pt-br/wiki/Porto_dos_Ventos'; // Substitua 'pagina.html' pelo URL real da página desejada
+							window.location.href = 'https://noiteterna-rpg.fandom.com/pt-br/wiki/' + cardName; // Substitua 'pagina.html' pelo URL real da página desejada
 						});
 					}
 				} else {
