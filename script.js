0055bd15d929c5b0bc48811ebb11dde0caa7b9e0
Smorcinski <pahblosmorcinski@gmail.com>
2024-05-25 03:22:47 -0300

    Atualizações: Atualização dos Cards

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
     var pois = document.querySelectorAll('.poi');
-        var cards = document.querySelectorAll('.card');
+	var cards = document.querySelectorAll('.card');
 
-        pois.forEach(function(poi) {
-            poi.addEventListener('click', function(event) {
-                event.stopPropagation();
-                var target = this.dataset.target;
-                cards.forEach(function(card) {
-                    if (card.id === target) {
-                        card.style.display = 'block';
-                    } else {
-                        card.style.display = 'none';
-                    }
-                });
-            });
-        });
+	pois.forEach(function(poi) {
+		poi.addEventListener('click', function(event) {
+			event.stopPropagation();
+			var target = this.dataset.target;
+			var mouseX = event.clientX;
+			var mouseY = event.clientY;
+			cards.forEach(function(card) {
+				if (card.id === target) {
+					card.style.display = 'block';
+					card.style.top = mouseY + 'px'; // Define a posição vertical do card
+					card.style.left = mouseX + 'px'; // Define a posição horizontal do card
+				} else {
+					card.style.display = 'none';
+				}
+			});
+		});
+	});
 
-        document.addEventListener('click', function(event) {
-            cards.forEach(function(card) {
-                card.style.display = 'none';
-            });
-        });
+	document.addEventListener('click', function(event) {
+		cards.forEach(function(card) {
+			card.style.display = 'none';
+		});
+	});
+
 });
