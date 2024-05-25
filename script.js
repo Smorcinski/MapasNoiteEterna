5c07012c7be616b3b8dae5db64d862964cfcba2d
Smorcinski <pahblosmorcinski@gmail.com>
2024-05-25 18:55:16 -0300

    Correções: Corrigindo nome de png

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     var pois = document.querySelectorAll('.poi');
     var cards = document.querySelectorAll('.card');
-    var overlays = document.querySelectorAll('.hidden-map-overlay');
 
     pois.forEach(function(poi) {
         poi.addEventListener('click', function(event) {
@@ -33,34 +32,6 @@ document.addEventListener('DOMContentLoaded', function () {
     document.addEventListener('click', function(event) {
         cards.forEach(function(card) {
             card.style.display = 'none';
-        });
-    });
-
-    // Adiciona funcionalidade de hover para as imagens de overlay
-    overlays.forEach(function(overlay) {
-        overlay.addEventListener('mouseenter', function(event) {
-            var rect = overlay.getBoundingClientRect();
-            var x = event.clientX - rect.left;
-            var y = event.clientY - rect.top;
-            var canvas = document.createElement('canvas');
-            var ctx = canvas.getContext('2d');
-            var img = new Image();
-            img.src = overlay.src;
-            img.onload = function() {
-                canvas.width = img.width;
-                canvas.height = img.height;
-                ctx.drawImage(img, 0, 0);
-                var pixelData = ctx.getImageData(x, y, 1, 1).data;
-                if (pixelData[3] > 0) { // Verifica a opacidade do pixel
-                    overlay.style.opacity = '1';
-                    overlay.style.pointerEvents = 'auto';
-                }
-            };
-        });
-
-        overlay.addEventListener('mouseleave', function() {
-            this.style.opacity = '0';
-            this.style.pointerEvents = 'none';
         });
     });
 });
