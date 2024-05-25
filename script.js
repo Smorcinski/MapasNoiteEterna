98c348ccc73fd3344d65d8f734a6af6a0b93ffb1
Smorcinski <pahblosmorcinski@gmail.com>
2024-05-25 03:16:04 -0300

    Atualizações: Atualização do index

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
     var pois = document.querySelectorAll('.poi');
-        var tooltip = document.getElementById('tooltip');
+        var cards = document.querySelectorAll('.card');
 
         pois.forEach(function(poi) {
             poi.addEventListener('click', function(event) {
-                event.stopPropagation(); // Impede a propagação do evento para evitar o fechamento imediato do tooltip
-                var info = this.dataset.info;
-                tooltip.textContent = info;
-                tooltip.style.display = 'block';
-                tooltip.style.width = '100px'; // Define a largura da caixa branca
-                tooltip.style.height = '50px'; // Define a altura da caixa branca
-                tooltip.style.top = (event.clientY - 60) + 'px'; // Posiciona a caixa branca verticalmente (ajuste para centralizar verticalmente)
-                tooltip.style.left = (event.clientX + 20) + 'px'; // Posiciona a caixa branca horizontalmente
+                event.stopPropagation();
+                var target = this.dataset.target;
+                cards.forEach(function(card) {
+                    if (card.id === target) {
+                        card.style.display = 'block';
+                    } else {
+                        card.style.display = 'none';
+                    }
+                });
             });
         });
 
         document.addEventListener('click', function(event) {
-            tooltip.style.display = 'none'; // Fecha o tooltip quando ocorre um clique fora dos POIs
+            cards.forEach(function(card) {
+                card.style.display = 'none';
+            });
         });
 });
